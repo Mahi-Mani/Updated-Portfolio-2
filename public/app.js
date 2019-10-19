4a685e18ae15e0d4271512ca56bd62b134b7182a
Mahi Mani <m.mahishamani@gmail.com>
2019-10-19 00:07:21 -0700

    Using handlebars created modals dynamically

--- public/app.js
+++ public/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-    $.ajax("/portfolio", {
-        type: "GET"
-    }).then(function(data){
-        console.log(data);
+    // $.ajax("/portfolio", {
+    //     type: "GET"
+    // }).then(function(data){
+    //     console.log(data);
+    // })
+    $(document).on("click", ".btn", function(event){
+        event.preventDefault();
+        console.log("Trigger modal button clicked!");
+        // jQuery.noConflict();
+        $("#train").modal("show");
+        // $("#train").modal();
     })
 })
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,16 +4,27 @@ var router = require('express').Router();
 
 // module.exports = function (app) {
 //   Portfolio route
+// router.get("/portfolio", function (req, res) {
+
+//     db.Portfolio.create({
+//         NAME: "Train Scheduler",
+//         IMAGE: "images/TrainSchedule.gif",
+//         DESC: "Train Scheduler application benefits user on showcasing schedules of different train user wishes to view. Usercan add new train at any point to view schedule. Minutes away field and next arrival keeps updating every minute so that user can keeptrack of their train.",
+//         LINK: "https://mahi-mani.github.io/TrainScheduler/"
+//     }).then(function (result) {
+//         console.log("New row created into portfolio table");
+//         res.json(result);
+//     })
+// })
+
 router.get("/portfolio", function (req, res) {
 
-    db.Portfolio.create({
-        NAME: "Train Scheduler",
-        IMAGE: "assets/images/TrainSchedule.gif",
-        DESC: "Train Scheduler application benefits user on showcasing schedules of different train user wishes to view. Usercan add new train at any point to view schedule. Minutes away field and next arrival keeps updating every minute so that user can keeptrack of their train.",
-        LINK: "https://mahi-mani.github.io/TrainScheduler/"
-    }).then(function (result) {
-        console.log("New row created into portfolio table");
-        res.json(result);
+    db.Portfolio.findAll({}).then(function (result) {
+        console.log(result);
+        var obj = {
+            result: result
+        }
+        res.render("index", obj);
     })
 })
 
